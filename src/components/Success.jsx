f22436387b4d1a9cb92cc8eb6d9ec73d708880e5
Ronak Jilka <ronakjilka@gmail.com>
2023-06-12 21:10:19 +0530

    first commit

---
+++ src/components/Success.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Successimg from "../assets/test.png";
+import "./Sucess.scss";
+
+export default function Success() {
+  return (
+    <div className="container-div">
+      <img src={Successimg} />
+    </div>
+  );
+}
